75cf432acaebfb732acdd263501542859730315c
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2021-10-10 19:39:17 -0400

    Framework for Buy and sell standard users
    created and added method overrides for BuySUser and SellSUser - DB connection not made

---
+++ BuySUser.py
@@ -0,0 +1,42 @@
+import user
+
+class BSUser(User):
+    """
+    Check if sellname exists
+        else throw error
+    check if sellName has title
+        else throw error
+    Check if numTickets <= 4
+        else deny transaction
+    check how many tickets remaining and if can sell numTickets to user
+        else throw error
+    display $/ticket
+    display total $
+    ask for confirmation of transaction
+    wait for response
+    decrease num ticket if user buys
+    """
+    def buy(self, title, numTickets, sellName):
+        #check if sellname exists
+        #insert db check for sell name
+        validSN = True
+        validTitle = True
+        titlePrice = 19.99
+        if(not validSN):
+            return "Invalid Seller";
+        if(not validTitle):
+            return "Invalid Title";
+        if(numTickets > 4):
+            return "Number of tickets bought cannot exceed 4"
+        remainingTick = 999-numTickets #999 needs to be replaced with remaining tickets from db
+        if(remainingTick >=0):
+            print("Price per Ticket: " +titlePrice +"\nTotal Price: " +titlePrice*numTickets)
+            userInput = input("Confirm Transaction Y/N")
+            #python has no switch case? switch case not implemented until 3.10
+            if(userInput == "Y" or userInput == "yes" or userInput == "Yes"):
+                #confirm transaction - decrease number of tickets from db - add to transaction line
+                print("Transaction Confirmed")
+                doStuff = 0
+            else:
+                print("Transaction Cancelled")
+        return 0
---
+++ SellSUser.py
@@ -0,0 +1,20 @@
+import user
+
+class SSUser(User):
+    def sell(self, title, price, numTickets):
+        """
+        check price is <= 999.99
+        check even title is of length <=25
+        check max ticket <= 100
+        ticket must not begin selling until after user logs out
+        """
+        if(price > 999.99):
+            return "Sell Price cannot exceed $999.99"
+        if(len(title) > 25):
+            return "Event Title cannot exceed 25 characters"
+        if(numTickets > 100):
+            return "Event cannot have more than 100 tickets"
+        #do stuff
+        #add to transaction file NOTE: since the event cant sell tickets until after the seller user logs off i think it might be best if we run a routine right before logging out when then adds the event
+        print("Event Created - " +"Event Name: " +title +"Ticket Price: " +price +" Number of tickets to be sold: " +numTickets)
+        return 0
