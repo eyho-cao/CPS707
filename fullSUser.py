ee8b70a964d83fa2d9e2edf8e24824f896cef396
AmielCastillo <46759229+AmielCastillo@users.noreply.github.com>
2021-10-12 15:20:44 -0400

    transaction string fix

--- fullSUser.py
+++ fullSUser.py
@@ -21,7 +21,7 @@ class FSUser(User):
            raise ValueError("Event cannot have more than 100 tickets")
         #do stuff
         #add to transaction file NOTE: since the event cant sell tickets until after the seller user logs off i think it might be best if we run a routine right before logging out that then adds the event
-        transaction = "03" + str(self.username + ("_" * (15 - len(self.username)))) + "_" + title + "_" + str(numTickets + ("_" * (3 - len(str(numTickets))))) + "_" + str(titlePrice + ("_" * (6 - len(str(titlePrice)))))
+        transaction = "03" + str(self.username + ("_" * (15 - len(self.username)))) + "_" + title + "_" + str(str(numTickets) + ("_" * (3 - len(str(numTickets))))) + "_" + str(str(titlePrice) + ("_" * (6 - len(str(titlePrice)))))
         f = open("daily_transaction_file.txt", "a") 
         f.write(transaction) 
         print("Event Created - " +"Event Name: " +title +"Ticket Price: " +price +" Number of tickets to be sold: " +numTickets)
@@ -47,7 +47,7 @@ class FSUser(User):
                     }}
 
                     eventCollection.update_one(eventQuery, remainingTick)
-                    transaction = "04" + str(self.username + ("_" * (15 - len(self.username)))) + "_" + title + "_" + str(numTickets + ("_" * (3 - len(str(numTickets))))) + "_" + str(titlePrice + ("_" * (6 - len(str(titlePrice)))))
+                    transaction = "04" + str(self.username + ("_" * (15 - len(self.username)))) + "_" + title + "_" + str(str(numTickets) + ("_" * (3 - len(str(numTickets))))) + "_" + str(str(titlePrice) + ("_" * (6 - len(str(titlePrice)))))
                     f = open("daily_transaction_file.txt", "a") 
                     f.write(transaction) 
                     print("Transaction Confirmed")
