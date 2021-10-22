388fa77804cbad8c2d465ca786f6f920a1e90554
Anthony Trinh <47074388+anthony-trinh@users.noreply.github.com>
2021-10-22 19:58:07 -0400

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -54,6 +54,47 @@ class Admin(User):
             raise ValueError('Username is not unique')
 
 
+    def createEvent(self, name, price, quantity, date, time, owner):
+
+        query = {"name": name}
+        result = collection.find_one(query)
+
+        #Check for validity of inputs 
+        if(result == None):
+            if(price > 0):
+                if(quantity > 0):
+                    if(isValidDate(date)):
+                        if(findUser(owner)):
+                            
+                            if("AM" in time.upper() or "PM" in time.upper()): time = formatTime(time)
+                            dateTime = formatDate(date, time) 
+                           
+                            
+                            #add the user to the database
+                            event = {
+                                "name": name,
+                                "price": price,
+                                "quantity": quantity,
+                                "datetime": dateTime,
+                                "owner": owner
+                            }
+
+                            collection.insert_one(event)
+
+                            #add this transaction to an output file... 
+                            #TODO
+                        else:
+                            raise ValueError("The owner does not exist")
+                    else:
+                        raise ValueError("The date entered is not valid")
+                else:
+                    raise ValueError("The quantity cannot be less than zero")
+            else:
+                raise ValueError("The price is invalid")
+        else:
+            raise ValueError("An event of the same name already exists")
+
+
     def buy(self, title, numTickets, sellName):
         return 0
 
@@ -140,3 +181,68 @@ class Admin(User):
     def checkUnique(username):
         #uneeded, this is handled in the User object constructor
         return 0
+
+
+
+#--------- HELPER FUNCTIONS ---------# 
+def isValidDate(date):
+    """
+    Checks if a date, date, is valid 
+    A valid date for an event will be past today's date 
+    It will also follow date conventions 
+    I.E. February 31st, 20__ is not a valid date 
+    """
+
+    var = date.split("/") 
+    today = str(datetime.datetime.now())[:10].split('-')
+
+    if(var[0] >= today[0]):
+        #check years 
+        if(var[1] >= today[1] and var[1] >= 1 and var[1] <= 12):
+            #check months 
+            if(var[2] >= today[2]):
+                #check days
+                if(var[1] in ['01','03','05','07','08','10','12']):
+                    if(var[2] <= '31'):
+                        return True 
+                elif(var[1] in ['04', '06', '09','11']):
+                    if(var[2] <= '30'):
+                        return True 
+                elif(var[1] == '2'):
+                    if(var[0] % 4 == 0 and var[2] <= '29'):
+                        #handles leap years 
+                        return True 
+                    if(var[2] <= '28'):
+                        return True 
+    return False 
+
+def formatTime(time):
+    """
+    Formats time into 24hr time 
+    """
+
+    ampm = time[:-3:-1][::-1]
+    _time = time.replace(" ","")
+    
+    if(ampm == 'am'):
+        return [int(_time[:-5]), int(time[-4:-2])]
+    else:
+        return [int(_time[:-5]) + 12, int(time[-4:-2])]
+
+def formatDate(date, time):
+    """
+    Formats a string representation of a date in the form "YYYY/MMM/DD" 
+    into a datetime object 
+    """
+
+    _date = date.split("/")
+    _time = formatTime(time)
+    return datetime.datetime(int(_date[0]), int(_date[1]), int(_date[2]), _time[0], _time[1])
+
+def findUser(username):
+    """
+    Check if a user with username exists
+    """
+    query = {"username": username}
+    
+    return collection.find_one(query)
